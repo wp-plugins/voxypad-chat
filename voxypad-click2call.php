b3ff3f9b63ea20e8fbc359fb6483b770378b9178
Voxypad <Voxypad@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-09 03:04:23 +0000

    update, remove the chat form
    git-svn-id: https://plugins.svn.wordpress.org/voxypad-chat/trunk@1195143 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- voxypad-click2call.php
+++ voxypad-click2call.php
@@ -1,33 +1,4 @@
 <?php
-define ( BLOCK_CSS,'
-    <style type="text/css">
-        .voxypad-click2call-block div {
-            display: inline-block;
-            margin-right: 20px;
-        }
-
-        .voxypad-click2call-block div img {
-            cursor: pointer;
-        }
-    </style>
-');
-
-define ( WIDGET_CSS,'
-    <style type="text/css">
-        .voxypad-click2call-widget div {
-            margin: 0 0 30px 0;
-            text-align: left !important;
-        }
-
-        .voxypad-click2call-widget div img {
-            width: auto !important;
-            height: 38px !important;
-            margin: 0 0 !important;
-            cursor: pointer;
-        }
-    </style>
-');
-
 /*
 Plugin Name: VoxyPAD Click2Call
 Plugin URI: https://voxypad.com/
@@ -38,51 +9,23 @@ Author URI: https://www.voxypad.com/
 */
 
 /**
- * VoxyPAD Shortcodes
- */
-add_action('init', 'voxypad_click2call_shortcodes');
-
-function voxypad_click2call_shortcodes() {
-    // Add CSS file
-    //wp_register_style('voxypad-click2call-css', plugins_url('/voxypad-click2call-style.css', __FILE__));
-    //wp_enqueue_style('voxypad-click2call-css');
-    // Register ShortCode
-    add_shortcode('voxypad-button', 'voxypad_click2call_button');
-}
-
-function voxypad_click2call_button($args, $content) {    
-    $content = '<div class="voxypad-click2call-block">';
-    // Render Button Voxy Chat Service: IF Attribute "voxychat" != "false" (default is "true")
-    if(isset($args['voxychat']) == FALSE || (isset($args['voxychat']) && $args['voxychat'] == 'true') ) {
-        $code_chat = get_option('voxypad_chat_service'); // get variable setting in form setting create above
-        $content .= $code_chat;
-    }
-    // Render Button Voxy Call Service: IF Attribute "voxycall" != "false" (default is "true")
-    if(isset($args['voxycall']) == FALSE || (isset($args['voxycall']) && $args['voxycall'] == 'true') ) {
-        $code_call = get_option('voxypad_call_service');// get variable setting in form setting create above
-        $content .= $code_call;
-    }
-    $content .= '</div>';
-    $content .= BLOCK_CSS;    
-
-    return $content;
-}
-/**
  * VoxyPAD Admin Settings
  */
 add_action('admin_menu', 'voxypad_click2call_plugin_menu');
 add_action('admin_init', 'voxypad_click2call_admin_init');
 
+// Define Menu Setting in WP Site
 function voxypad_click2call_plugin_menu() {
     add_options_page('VoxyPAD Click2Call Options', 'VoxyPAD Click2Call', 'manage_options',
         'voxypad_click2call', 'voxypad_click2call_plugin_options');
 }
 
+// Define a Variable to save Setting
 function voxypad_click2call_admin_init() {
-    register_setting('voxypad-click2call-services', 'voxypad_chat_service');
-    register_setting('voxypad-click2call-services', 'voxypad_call_service');
+    register_setting('voxypad-click2call-services', 'voxypad_box_code');
 }
 
+// Create form to get setting option
 function voxypad_click2call_plugin_options() {
     ?>
     <section class="voxypad-click2call-settings">
@@ -93,30 +36,31 @@ function voxypad_click2call_plugin_options() {
             <?php @do_settings_fields('voxypad-click2call-services');?>
             <table class="form-table">
                 <tr valign="top">
-                    <th scope="row"><label for="voxypad_chat_service">VoxyPAD Chat</label> </th>
+                    <th scope="row"><label for="voxypad_box_code">Enter chat or voice box code:</label> </th>
                     <td>
-                        <textarea rows="5" class="large-text code" id="voxypad_chat_service" name="voxypad_chat_service"
-                                  placeholder="Paste code here..."><?php echo get_option('voxypad_chat_service');?></textarea>
-                        <br/><small>Code is supported by <a href="https://voxypad.com/business">VoxyPAD Service</a> </small>
+                        <textarea rows="5" class="large-text code" id="voxypad_box_code" name="voxypad_box_code"
+                                  placeholder="Paste code here..."><?php echo get_option('voxypad_box_code');?></textarea>
+                        <br/>
+                        <ul style="list-style:inside none disc;">
+                            <li><small>Insert the generated code. 
+                    To get the code, login into <a title="VoxyPAD Website" href="https://voxypad.com">Voxypad.com</a> web site. 
+                    Upon logged in, in the <b>Dashboard</b>, cut and paste the <b>"Chat box"</b> or <b>"Call box"</b> code.<br> If the code is not existing, select <b>"New Order"</b> to generate the code. 
+                            Please, insert only one code (either chat or call box). 
+                            The call box code will also include the chat service.</small></li>
+                        </ul>
                     </td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row"><label for="voxypad_call_service">VoxyPAD Call</label> </th>
-                    <td>
-                        <textarea rows="5" class="large-text code" id="voxypad_call_service" name="voxypad_call_service"
-                                  placeholder="Paste code here..."><?php echo get_option('voxypad_call_service');?></textarea>
-                        <br/><small>Code is supported by <a href="https://voxypad.com/business">VoxyPAD Service</a> </small>
-                    </td>
-                </tr>
+                </tr>               
             </table><?php @submit_button();?>
-        </form>
+        </form>        
     </section>
     <?php
 }
+
 /**
  * VoxyPAD Widget
  */
 add_action('widgets_init', 'voxypad_widget_init');
+
 // Register Widget
 function voxypad_widget_init() {
     register_widget(Voxypad_Widget);
@@ -135,16 +79,11 @@ class Voxypad_Widget extends WP_Widget {
      */
     function widget($args, $instance) {
         $content = '<aside class="voxypad-click2call-widget widget widget_voxypad">';
-        // Render Button Voxy Chat Service
-        $code_chat = get_option('voxypad_chat_service');
-        $content .= $code_chat;
-        // Render Button Voxy Call Service
-        $code_call = get_option('voxypad_call_service');
-        $content .= $code_call;
+        // Render Box Code
+        $box_code = get_option('voxypad_box_code');
+        $content .= $box_code;       
 
         $content .= '</aside>';
-
-        $content .= WIDGET_CSS;
         
         echo $content;
     }
